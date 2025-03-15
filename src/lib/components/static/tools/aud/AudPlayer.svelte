a99512f38569bf3a392071272cc5296218aa9539
zempo <zelenkosolomon@gmail.com>
2025-03-15 11:13:27 -0700

    update aud

--- src/lib/components/static/tools/aud/AudPlayer.svelte
+++ src/lib/components/static/tools/aud/AudPlayer.svelte
@@ -3,6 +3,11 @@
 
 	let { audData, selectedAud = 0 } = $props();
 	const { type, title, src, timestamps, notes } = audData[selectedAud];
+	let playState = $state({
+		time: 0,
+		duration: 0,
+		paused: true
+	});
 	// src,title,type,timestamps,notes
 
 	//  getAllContexts
@@ -11,30 +16,36 @@
 <!-- player + tracklist container -->
 <div class="aud_wrap">
 	<div class="aud_header">
-		<h2>{title}</h2>
+		<h3>{title}</h3>
 	</div>
-	{audPlay.setAud(audData, selectedAud)}
 	<div class="aud_player">
-		<div class="aud_stats">
-			<span class="time"></span>
-			<div class="slider"></div>
-			<div class="progress"></div>
-		</div>
-		<div class="aud_controls">
-			<button>Play</button>
-		</div>
+		<audio {src}></audio>
+		{@render audBody()}
 	</div>
 	<div class="aud_tracklist">
 		<ul>
-			{#each audData as A}
-				{@render trackList(A)}
-			{/each}
+			{#each audData as A}{@render trackList(A)}{/each}
 		</ul>
 	</div>
 </div>
 
+<!-- ??? AudBody -->
+{#snippet audBody()}
+	<div class="aud_stats">
+		<span class="time">{audPlay.format(playState.time)}</span>
+		<div class="slider"></div>
+		<span class="progress"></span>
+	</div>
+	<div class="aud_controls">
+		<button>Play</button>
+	</div>
+{/snippet}
+
+<!-- ??? TrackList -->
 {#snippet trackList(A)}
 	<li class={`aud_item is_selected_aud?`}>
-		{A.title}
+		<button class="track_btn">
+			{A.title}
+		</button>
 	</li>
 {/snippet}
