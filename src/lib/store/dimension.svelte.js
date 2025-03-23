037b7c190edad6de2658cfc01d34dc028121fc82
zempo <zelenkosolomon@gmail.com>
2025-03-23 10:48:47 -0700

    mod

--- src/lib/store/dimension.svelte.js
+++ src/lib/store/dimension.svelte.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef */
 class Dimension {
 	topbarH = $state(60);
+	hideTop = $state(false);
 	currScrollLanding = $state(0);
 	currW = $state(300);
 	trunc1 = $derived(Math.ceil(this.currW / 24));
@@ -12,6 +13,19 @@ class Dimension {
 			top: 0,
 			behavior: 'smooth'
 		});
+	}
+
+	showTopbar() {
+		let currScroll = 0;
+		let cond1 = window.innerHeight + window.scrollY;
+		let cond2 = document.body.offsetHeight - 1;
+		if (cond1 >= cond2) {
+			this.hideTop = false;
+		} else if (currScroll) {
+			this.hideTop = true;
+		} else {
+			this.hideTop = false;
+		}
 	}
 
 	calcFeatured() {
