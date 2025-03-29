8fa0472165a1976f797c3d64bc12e63fa012650a
zempo <zelenkosolomon@gmail.com>
2025-03-29 16:39:29 -0700

    update table

--- src/lib/components/layout/PgHero.svelte
+++ src/lib/components/layout/PgHero.svelte
@@ -3,6 +3,7 @@
 	import { taglines, titles } from '$lib/config';
 	import { dimension } from '$lib/store/dimension.svelte';
 	import { main } from '$lib/store/main.svelte';
+	import { formatTimestampLong } from '../content/helpers/api';
 
 	let { regHero = 'pg', meta } = $props();
 </script>
@@ -15,6 +16,8 @@
 {:else if regHero === 'post'}
 	<header class="{main.getKey()}_hero post_hero">
 		<h1>{meta.title}</h1>
-		<!-- <p class="sub_title">{taglines[main.getKey()]}</p> -->
+		<div class="hero_wrap">
+			<p class="sub_title"><b>Published:</b> {@html formatTimestampLong(meta.created_at)}</p>
+		</div>
 	</header>
 {/if}
--- src/routes/bytes/+layout.svelte
+++ src/routes/bytes/+layout.svelte
@@ -2,8 +2,15 @@
 	import ApiAside from '$lib/components/content/helpers/ApiAside.svelte';
 	import PgWrap from '$lib/components/layout/PgWrap.svelte';
 	import { searchIcon } from '$lib/components/static/svg/nav.js';
+	// import { page } from '$app/state';
 
 	const { children, data } = $props();
+
+	// let url = $page.url;
+	// let urlTopic = null;
+	// $effect.pre(() => {
+	// 	urlTopic = url.searchParams.get('topic')
+	// })
 
 	const {
 		labs,
@@ -19,15 +26,25 @@
 	} = data;
 </script>
 
+{#snippet searchForm()}
+	<form autocomplete="off" role="search" class="search_form">
+		<label for="byte_search" class="sr">Filter Bytes by Keyword</label>
+		<input id="byte_search" class="search_input" placeholder="Explore Art" type="search" />
+		<button type="submit" title="Search" aria-label="Submit search" class="search_btn"
+			>{@html searchIcon}</button
+		>
+	</form>
+{/snippet}
+
+{#snippet allTagsList(T)}
+	<ul class="all_topics_list">
+		{@render allTagsList()}
+	</ul>
+{/snippet}
+
 <PgWrap type="w">
-	{@render children()}
 	<ApiAside>
-		<form autocomplete="off" role="search" class="search_form">
-			<label for="byte_search" class="sr">Filter Bytes by Keyword</label>
-			<input id="byte_search" class="search_input" placeholder="Explore Art" type="search" />
-			<button type="submit" title="Search" aria-label="Submit search" class="search_btn"
-				>{@html searchIcon}</button
-			>
-		</form>
+		{@render searchForm()}
 	</ApiAside>
+	{@render children()}
 </PgWrap>
