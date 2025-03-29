e613f23b115079119bb478b98bf2a6bfdfd35930
zempo <zelenkosolomon@gmail.com>
2025-03-29 15:04:01 -0700

    update text

--- src/lib/components/content/helpers/ApiCard.svelte
+++ src/lib/components/content/helpers/ApiCard.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { byteLab, byteLab2, byteLab3 } from '$lib/components/static/svg/api';
+	import { byteKit, byteLab } from '$lib/components/static/svg/api';
 	import { formatTimestampLong } from './api';
 
 	const { content = [], type = 'lab' } = $props();
@@ -12,19 +12,42 @@
 			excerpt: s.excerpt,
 -->
 
-{#snippet contentLink(L)}
-	<a data-sveltekit-preload-data href="/bytes/{type}/{L.slug}">{L.title}</a>
+{#snippet contentLink(L, rdm = 1)}
+	{#if rdm === 1}
+		<a data-sveltekit-preload-data href="/bytes/{type}/{L.slug}">{L.title}</a>
+	{:else}
+		<a data-sveltekit-preload-data href="/bytes/{type}/{L.slug}">{rdm}</a>
+	{/if}
+{/snippet}
+
+{#snippet tagList(T)}
+	<li class="tag_item">
+		<a href="/bytes?topic={T}">#{T}</a>
+	</li>
 {/snippet}
 
 {#snippet contentItem(L, idx)}
 	<li class="byte_item">
 		<article class="byte_preview {type}_preview">
 			<div class="prev_head">
-				<div class="gl">{@html byteLab}</div>
+				<div class="gl">
+					{#if type === 'lab'}{@html byteLab}{:else}{@html byteKit}{/if}
+				</div>
 				<div class="gr">
 					<h2 class="use_h4">{@render contentLink(L)}</h2>
 					<p class="pub">{@html formatTimestampLong(L.created_at)}</p>
 				</div>
+			</div>
+			<div class="prev_body">
+				<p class="excerpt">
+					{L.excerpt}
+					{@render contentLink(L, 'Read More...')}
+				</p>
+				<ul class="tag_list">
+					{#each L.topics as T, idx}
+						{@render tagList(T)}
+					{/each}
+				</ul>
 			</div>
 		</article>
 	</li>
